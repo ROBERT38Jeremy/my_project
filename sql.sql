6ab785d50ee0eb0bcf688034563c835184673430
JRobert <jeremy100401@gmail.com>
2022-12-06 13:58:21 +0100

    correction des triggers

--- sql.sql
+++ sql.sql
@@ -2,16 +2,16 @@
 DELIMITER //
 CREATE PROCEDURE insert_serie(IN p_nom VARCHAR(50), IN p_plateforme INT)
 BEGIN
-	INSERT INTO serie (nom, platefrome) VALUES (p_nom, p_plateforme);
+	INSERT INTO serie (nom, platefrome_id) VALUES (p_nom, p_plateforme);
 END //
 
 -- TRIGGERS
 DELIMITER //
 CREATE TRIGGER suppression_auto_saison
-AFTER DELETE ON serie FOR EACH ROW
+BEFORE DELETE ON serie FOR EACH ROW
     DELETE FROM saison WHERE saison.serie_id = OLD.id;
 //
 CREATE TRIGGER suppression_auto_episode
-AFTER DELETE ON saison FOR EACH ROW
+BEFORE DELETE ON saison FOR EACH ROW
     DELETE FROM episode WHERE episode.saison_id = OLD.id;
 //
